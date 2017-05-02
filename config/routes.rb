fedfc756c1017799b38689e1a1745a11a7fc1fc3
Faber Santos <faber@dynamicfactor.com>
2017-05-02 21:46:53 +0800

    Home Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+#  get 'home/index'
+
+#  get 'home/exchange'
+
+  root 'home#index'
+  post 'exchange', to: 'home#exchange'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
